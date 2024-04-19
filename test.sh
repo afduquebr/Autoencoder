060cbf72e7d9622b61a0ce6def04fdb7e9f5b99c
afduquebr <afduquebr@unal.edu.co>
2024-04-19 16:34:10 +0200

    Adding testing to git automated

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ if ! source .venv/bin/activate; then
 fi
 
 # Define variables for training
-scale="standard"
-middle_dim="84"
+scale="minmax"
+middle_dim="21"
 latent_dim="14" 
 
 # Run Testing Python script
@@ -23,6 +23,22 @@ fi
 echo "Plotting histograms."
 if ! python hist.py -p local -s $scale -m $middle_dim -l $latent_dim; then
     echo "Error: Failed to run Histogram Python script."
+    exit 1
+fi
+
+# Update Git repository automatically
+if ! git add .; then
+    echo "Error: Failed to add files to Git."
+    exit 1
+fi
+
+if ! git commit -m "Testing with $scale scaling and $middle_dim, $latent_dim layer dimensions"; then
+    echo "Error: Failed to commit changes to Git."
+    exit 1
+fi
+
+if ! git push; then
+    echo "Error: Failed to push changes to Git repository."
     exit 1
 fi
 
