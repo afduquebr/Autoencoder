22798e4dbfa2f6314e44218f2f4825632db027bf
Duque Bran Andres Felipe <duquebran@clratlserv09.in2p3.fr>
2024-06-12 10:38:26 +0200

    Training with bbox insertion and percentage 0.005

--- train.sh
+++ train.sh
@@ -17,7 +17,7 @@ cd /AtlasDisk/home2/duquebran/Autoencoder/ || exit
 
 # Define variables for training
 path="server"
-dataset="sig1"
+dataset="sig2"
 anomaly="0.005"
 
 # Run Training Python script
