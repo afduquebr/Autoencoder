3fcd8d843663d30db5d5864bb4e9c2b7a34c7819
Duque Bran Andres Felipe <duquebran@clratlserv09.in2p3.fr>
2024-06-13 10:53:17 +0200

    Testing with sig1 insertion and percentage 0.005

--- test.py
+++ test.py
@@ -134,7 +134,7 @@ nbins = 40
 fig, axes = plt.subplots(figsize=(8,6))
 axes.hist([loss_sample_total], nbins, range=(0, 1.5), density=1, histtype='step', label=['Data'], stacked=True, alpha=1)
 axes.hist([loss_bkg_total], nbins, range=(0, 1.5), density=1, histtype='step', label=['Background'], stacked=True, alpha=0.9)
-axes.hist([loss_sig_total], nbins, range=(0, 1.5), density=1, histtype='step', label=['Signal: ' + signal], stacked=True, alpha=0.8)
+axes.hist([loss_sig_total], nbins, range=(0, 1.5), density=1, histtype='step', label=['Signal'], stacked=True, alpha=0.8)
 axes.set_xlabel(r"Reconstruction Error")
 axes.set_ylabel("Events")
 axes.set_xlim(0, 1.5)
