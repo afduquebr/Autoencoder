5d26e7d7a0a9ab79fd56526e875822c26aa4068a
Duque Bran Andres Felipe <duquebran@clratlserv09.in2p3.fr>
2024-06-13 10:52:06 +0200

    Testing with sig1 insertion and percentage 0.005

--- test.py
+++ test.py
@@ -58,6 +58,7 @@ bbox = bbox[(bbox[mass] > scope[0]) & (bbox[mass] < scope[1])].reset_index()
 # Mix signal or bbox with bkg
 
 if signal != None:
+    sample_bkg = bkg.sample(frac=1)
     sample_sig = globals()[signal].sample(frac=1)
     sample = pd.concat([bkg, sample_sig[:int(pct * len(bkg))]]).sample(frac=1)
 else:
@@ -67,24 +68,14 @@ else:
     sample = bkg.sample(frac=1)
 
 mjj_sample = sample[mass].values
+mjj_bkg = sample_bkg[mass].values
 mjj_sig = sample_sig[mass].values
-
-#######################################################################################################
-############################################# Reweighting #############################################
-
-Hc,Hb = np.histogram(mjj_sample,bins=500)
-weights = np.array(Hc,dtype=float)
-weights[weights > 0.0] = 1.0 / weights[weights > 0.0]
-weights[weights == 0.0] = 1.0
-weights = np.append(weights, weights[-1])
-weights *= 1000.0 # To avoid very small weights
-weights = weights[np.searchsorted(Hb, mjj_sample)]
 
 #######################################################################################################
 ######################################## Data Preprocessing ###########################################
 
 # Concatenate all datasets for the current column to find the global min and max
-all_data = pd.concat([sample[selection], sample_sig[selection]])
+all_data = pd.concat([sample[selection], sample_bkg[selection], sample_sig[selection]])
 
 for col in smooth_cols:
     first_positive = all_data[col][all_data[col] > 0].min()
@@ -98,15 +89,16 @@ data_scaled = pd.DataFrame(scaler.fit_transform(all_data), columns=selection)
 
 # Apply scaling to each dataset per column
 sample_scaled = data_scaled.iloc[:len(sample)]
-sig_scaled = data_scaled.iloc[len(sample):]
+bkg_scaled = data_scaled.iloc[len(sample):-len(sample_sig)]
+sig_scaled = data_scaled.iloc[-len(sample_sig):]
 
 #######################################################################################################
 ######################################### Data Rescaling ##############################################
 
 test_sample = torch.from_numpy(sample_scaled[:100000].values).float().to(device)
+test_bkg = torch.from_numpy(bkg_scaled[:100000].values).float().to(device)
 test_sig = torch.from_numpy(sig_scaled.values).float().to(device)
-weights = torch.from_numpy(weights[:100000]).float().to(device)
-mjj = torch.from_numpy(mjj_sample[:100000]).float().to(device)
+# mjj = torch.from_numpy(mjj_sample[:100000]).float().to(device)
 
 #######################################################################################################
 ########################################## Testing Analysis ############################################
@@ -122,23 +114,27 @@ model.eval()
 # Predictions
 with torch.no_grad(): # no need to compute gradients here
     predict_sample = model(test_sample)
+    predict_bkg = model(test_bkg)
     predict_sig = model(test_sig)
 
 # Determine Reconstruction Error
 
 # MSE per feature
 loss_sample = pd.DataFrame(loss(test_sample, predict_sample).numpy(), columns=selection)
+loss_bkg = pd.DataFrame(loss(test_bkg, predict_bkg).numpy(), columns=selection)
 loss_sig = pd.DataFrame(loss(test_sig, predict_sig).numpy(), columns=selection)
 
 # Total MSE
 loss_sample_total = loss_sample.mean(axis=1)
+loss_bkg_total = loss_bkg.mean(axis=1)
 loss_sig_total = loss_sig.mean(axis=1)
 
 # Plot Total Reconstruction Error
 nbins = 40
 fig, axes = plt.subplots(figsize=(8,6))
-axes.hist([loss_sample_total], nbins, range=(0, 1.5), density=1, histtype='step', label=['Background'], stacked=True, alpha=1)
-axes.hist([loss_sig_total], nbins, range=(0, 1.5), density=1, histtype='step', label=['Signal: ' + signal], stacked=True, alpha=0.9)
+axes.hist([loss_sample_total], nbins, range=(0, 1.5), density=1, histtype='step', label=['Data'], stacked=True, alpha=1)
+axes.hist([loss_bkg_total], nbins, range=(0, 1.5), density=1, histtype='step', label=['Background'], stacked=True, alpha=0.9)
+axes.hist([loss_sig_total], nbins, range=(0, 1.5), density=1, histtype='step', label=['Signal: ' + signal], stacked=True, alpha=0.8)
 axes.set_xlabel(r"Reconstruction Error")
 axes.set_ylabel("Events")
 axes.set_xlim(0, 1.5)
@@ -148,8 +144,8 @@ fig.savefig(f"figs/testing/reconstruction_error_{signal}_{(int(pct * 1000) % 100
 ############################################ ROC Curve ##############################################
 
 
-loss_total = pd.concat([loss_sample_total, loss_sig_total], axis=0, ignore_index=1)
-labels = pd.Series([0]*len(loss_sample_total) + [1]*len(loss_sig_total))
+loss_total = pd.concat([loss_bkg_total, loss_sig_total], axis=0, ignore_index=1)
+labels = pd.Series([0]*len(loss_bkg_total) + [1]*len(loss_sig_total))
 loss_total = pd.DataFrame({'Loss': loss_total, 'Label': labels})
 
 fpr, tpr, thresholds = roc_curve(loss_total["Label"], loss_total["Loss"])
@@ -162,8 +158,8 @@ axes.plot(fpr, tpr, lw=2, label='Signal ROC curve (AUC = %0.2f)' % roc_auc)
 axes.plot([0, 1], [0, 1], lw=2, linestyle='--')
 axes.set_xlim([0.0, 1.0])
 axes.set_ylim([0.0, 1.05])
-axes.set_xlabel('False Positive Rate')
-axes.set_ylabel('True Positive Rate')
+axes.set_xlabel('Signal Acceptance')
+axes.set_ylabel('Backgroung Acceptance')
 axes.set_title('Receiver Operating Characteristic (ROC) Curve')
 axes.legend(loc="lower right")
 fig.savefig(f"figs/testing/ROC_{signal}_{(int(pct * 1000) % 100):02d}.png")
@@ -179,7 +175,7 @@ nbins = 30
 fig, axes = plt.subplots(figsize=(8,6))
 axes.hist([mjj_sample[:100000]], nbins, range=(2700, 5000), density=1, histtype='step', label=['No selection'], stacked=True, alpha=0.6)
 axes.hist([mjj_sample[:100000][loss_sample_total > threshold[70 - 1]]], nbins, range=(2700, 5000), density=1, histtype='step', label=['70%'], stacked=True, alpha=0.8)
-axes.hist([mjj_sample[:100000][loss_sample_total > threshold[95 - 1]]], nbins, range=(2700, 5000), density=1, histtype='step', label=['95%'], stacked=True, alpha=1)
+axes.hist([mjj_sample[:100000][loss_sample_total > threshold[90 - 1]]], nbins, range=(2700, 5000), density=1, histtype='step', label=['90%'], stacked=True, alpha=1)
 axes.set_xlabel(r"$m_{jet_1•jet_2}$ [GeV]")
 axes.set_ylabel("Events")
 axes.set_xlim(2700, 5000)
@@ -220,21 +216,26 @@ fig.savefig(f"figs/testing/error_{signal}_{(int(pct * 1000) % 100):02d}.png")
 _, bins = np.histogram(mjj_sample[:100000], bins=50, range=(2700, 5000))
 
 loss_sample_avg = []
+loss_bkg_avg = []
 loss_sig_avg = []
 
 for i in range(len(bins) - 1):
     loss_sample_bin = loss_sample_total[(mjj_sample[:100000] >= bins[i]) & (mjj_sample[:100000] < bins[i + 1])]
+    loss_bkg_bin = loss_bkg_total[(mjj_bkg[:100000] >= bins[i]) & (mjj_bkg[:100000] < bins[i + 1])]
     loss_sig_bin = loss_sig_total[(mjj_sig >= bins[i]) & (mjj_sig < bins[i + 1])]
 
     loss_sample_bin_avg = np.mean(loss_sample_bin)
+    loss_bkg_bin_avg = np.mean(loss_bkg_bin)
     loss_sig_bin_avg = np.mean(loss_sig_bin)
 
     loss_sample_avg.append(loss_sample_bin_avg)
+    loss_bkg_avg.append(loss_bkg_bin_avg)
     loss_sig_avg.append(loss_sig_bin_avg)
 
 # Plot Avg Loss v. Mass
 fig, axes = plt.subplots(figsize=(8,6))
-axes.plot(bins[:-1] + np.diff(bins) / 2, loss_sample_avg, label='Background')
+axes.plot(bins[:-1] + np.diff(bins) / 2, loss_sample_avg, label='Data')
+axes.plot(bins[:-1] + np.diff(bins) / 2, loss_bkg_avg, label='Background')
 axes.plot(bins[:-1] + np.diff(bins) / 2, loss_sig_avg, label='Signal')
 axes.set_xlim([2700, 5000])
 axes.set_xlabel(r"$m_{jet_1•jet_2}$")
@@ -242,14 +243,3 @@ axes.set_ylabel('Reconstruction Error')
 axes.set_title('Avg Error v. Mass Distribution')
 axes.legend()
 fig.savefig(f"figs/testing/AvgLossMass_{signal}_{(int(pct * 1000) % 100):02d}.png")
-
-
-#########
-
-# nbins = 50
-# fig, axes = plt.subplots(figsize=(8,6))
-# axes.hist([mjj_bkg], nbins, histtype='step', weights=weights_bkg.cpu().numpy(), label=['Background'], stacked=True, alpha=1)
-# axes.set_xlabel(r"$m_{jet_1•jet_2}$")
-# axes.set_ylabel("Events")
-# axes.legend()
-# fig.savefig(f"figs/testing/normalised_mass_dist_{scale}_{mid_dim}_{latent_dim}.png")
