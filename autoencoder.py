e9cda199d1fc910e8ee78fc34c8d1cce5019da96
afduquebr <afduquebr@unal.edu.co>
2024-07-24 17:27:36 +0200

    Delete unnecessary packages

--- autoencoder.py
+++ autoencoder.py
@@ -6,15 +6,8 @@ Created on Mar 25 2024
 """
 #######################################################################################################
 
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader, TensorDataset
-import torchvision
 
 from Disco import distance_corr
 
